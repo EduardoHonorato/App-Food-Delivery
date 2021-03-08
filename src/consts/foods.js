3c4c46a217b41daffdab70e689512bfc546ddc56
EduardoHonorato <eduardo_pk@live.com>
2021-03-08 14:28:24 -0300

    first commit

---
+++ src/consts/foods.js
@@ -0,0 +1,32 @@
+const foods = [
+  {
+    id: '1',
+    name: 'Pizza de peperoni',
+    ingredients: 'Mixed Pizza',
+    price: '28,50',
+    image: require('../assets/meatPizza.png'),
+  },
+  {
+    id: '2',
+    name: 'Pizza de queijo',
+    ingredients: 'Molho, queijo...',
+    price: '30,00',
+    image: require('../assets/cheesePizza.png'),
+  },
+  {
+    id: '3',
+    name: 'Hamburger',
+    ingredients: 'Pão, maionese...',
+    price: '22,00',
+    image: require('../assets/chickenBurger.png'),
+  },
+  {
+    id: '4',
+    name: 'Sushi',
+    ingredients: 'Combinado de sushi',
+    price: '39,90',
+    image: require('../assets/sushiMakizushi.png'),
+  },
+];
+
+export default foods;
